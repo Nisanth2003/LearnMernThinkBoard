6501ef6be64bd4d6df0333bf3fd50358c9b44f63
Nisanth <nisanthreddymedapati@gmail.com>
2026-03-22 00:16:40 +0530

    day-4-2

--- backend/src/routes/NotesRoutes.js
+++ backend/src/routes/NotesRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getStatus, getStatus1 } from "../../controllers/StatusController";
+import { getStatus, getStatus1 } from "../controllers/StatusController.js";
 
 
-const router = express.router();
+const router = express.Router();
 
 router.get("/", (req, res) => {
     res.status(200).json({message:"Hello World"});
